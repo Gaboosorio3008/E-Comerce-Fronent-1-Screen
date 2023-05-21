b0041c6172ef1544f041b30d896695edfb89a155
Gabriel Osorio <Gabrielosorio.3008@gmail.com>
2023-05-21 05:19:53 -0400

    screen 1 practice to tailwindcss

--- App.js
+++ App.js
@@ -1,16 +1,30 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Switch, Text, View } from 'react-native';
+import { useColorScheme } from 'nativewind';
+import ProductList from './components/productList';
 
 export default function App() {
+  const { colorScheme, toggleColorScheme } = useColorScheme()
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+    <SafeAreaView className=" flex-2 justify-center max-w-full items-center  bg-gray-200 dark:bg-black p-12 mb-5"  >
+      <View className="flex-row max-w-full gap-10 mt-10">
+        <Text className="text-2xl font-bold dark:text-white">
+          New Collection
+        </Text>
+        <Switch value={colorScheme === "dark"} onChange={toggleColorScheme} />
+      </View>
+      <View className="max-w-full gap-3 mb-10">
+
+      <ProductList className="max-w-full gap-3"/>
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView> 
   );
 }
 
-const styles = StyleSheet.create({
+/* const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
@@ -18,3 +32,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+ */
---
+++ products.js
@@ -0,0 +1,35 @@
+export const products = [
+    {
+        id: 1,
+        title: 'Pantalon',
+        price: 14,
+        image: 'https://ae01.alicdn.com/kf/Hd4c847e6ca73455789ad4eb093a787a1C/Pantalones-vaqueros-rasgados-de-Streetwear-para-mujer-pantalones-vaqueros-ajustados-negros-desgastados-el-sticos-Push-Up.jpg_Q90.jpg_.webp',
+        category: 'Ropa',
+        description: 'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. '
+    },
+    {
+        id: 2,
+        title: 'Pantalon',
+        price: 20,
+        image: 'https://ae01.alicdn.com/kf/HTB1wYTmaUvrK1RjSszfq6xJNVXaC.jpg',
+        category: 'Ropa',
+        description: 'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. '
+    },
+    {
+        id: 3,
+        title: 'Camisa',
+        price: 20,
+        image: 'https://ae01.alicdn.com/kf/Hfca4813745324322a6f408d36a79d779H/Blusa-de-gasa-de-manga-larga-para-mujer-camisa-blanca-con-cuello-vuelto-a-la-moda.jpg_Q90.jpg_.webp',
+        category: 'Ropa',
+        description: 'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. '
+    },
+    {
+        id: 4,
+        title: 'Camisa',
+        price: 12,
+        image: 'https://ae01.alicdn.com/kf/Hf1a14dd6206144f5944705bfe8a946e3V/Blusa-De-manga-larga-para-Mujer-Camisa-De-gasa-blanca-con-cuello-alto-2021.jpg_640x640.jpg',
+        category: 'Ropa',
+        description: 'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. '
+    }
+
+]
